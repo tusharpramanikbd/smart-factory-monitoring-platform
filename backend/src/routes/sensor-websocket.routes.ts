4648563e0bf7f32ad21ae23c4cfac9974d6fbded
Tushar Pramanik <tusharpramanikbd@gmail.com>
2026-06-06 11:30:37 +0530

    added fast snapshot to websocket

--- backend/src/routes/sensor-websocket.routes.ts
+++ backend/src/routes/sensor-websocket.routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 
 import { addClient, removeClient } from "../services/websocket.service";
+import { getLatestReading } from "../services/sensor-stream.service";
 
 import { MachineWebSocketParams } from "../types/websocket-route.types";
+import { SensorUpdateMessage } from "../types/websocket-message.types";
 
 export async function sensorWebSocketRoutes(app: FastifyInstance) {
   app.get("/ws/sensors/:machineId", { websocket: true }, (socket, request) => {
@@ -11,6 +13,17 @@ export async function sensorWebSocketRoutes(app: FastifyInstance) {
     console.log(`Client connected to machine ${machineId}`);
 
     addClient(machineId, socket);
+
+    const latestReading = getLatestReading(machineId);
+
+    if (latestReading) {
+      const message: SensorUpdateMessage = {
+        type: "sensor-update",
+        data: [latestReading],
+      };
+
+      socket.send(JSON.stringify(message));
+    }
 
     socket.on("close", () => {
       console.log(`Client disconnected from machine ${machineId}`);
